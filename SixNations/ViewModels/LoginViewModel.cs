806458651e99ae02f9b7f4df0aaec14b541c38b9
savaged <david@savaged.info>
2018-08-22 10:09:09 +0100

    Auth login coming together

--- SixNations/ViewModels/LoginViewModel.cs
+++ SixNations/ViewModels/LoginViewModel.cs
@@ -1,36 +1,53 @@
 ﻿using System;
 using GalaSoft.MvvmLight;
-using System.Threading.Tasks;
 using System.Windows.Input;
 using GalaSoft.MvvmLight.Command;
+using Windows.Storage;
 
 namespace SixNations.ViewModels
 {
     public class LoginViewModel : ViewModelBase
     {
         private string _email;
+        private RelayCommand<object> _submitCmd;
 
         public LoginViewModel()
         {
-            _email = string.Empty;
-            SubmitCmd = new RelayCommand<string>(OnSubmit, (b) => CanSubmit);
+            _email = (string)ApplicationData.Current.LocalSettings.Values["user_email"];
+
+            _submitCmd = new RelayCommand<object>(OnSubmit, (p) => CanExecute);
         }
 
-        public ICommand SubmitCmd { get; set; }
+        public ICommand SubmitCmd
+        {
+            get => _submitCmd;
+            set => _submitCmd = value != null ? (RelayCommand<object>)value : null;
+        }
 
-        public bool CanSubmit => !string.IsNullOrEmpty(Email);
+        public bool CanExecute
+        {
+            get
+            {
+                var canExecute = !string.IsNullOrEmpty(Email);
+                return canExecute;
+            }
+        }
 
         public string Email
         {
             get => _email;
             set
             {
-                Set(ref _email, value);
-                RaisePropertyChanged(nameof(CanSubmit));
+                if (_email != value)
+                {
+                    Set(ref _email, value);
+                    ApplicationData.Current.LocalSettings.Values["user_email"] = value;
+                    _submitCmd.RaiseCanExecuteChanged();
+                }
             }
         }
 
-        private void OnSubmit(string password)
+        private void OnSubmit(object password)
         {
             throw new NotImplementedException();
         }
