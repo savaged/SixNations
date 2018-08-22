c1dfda273ba656dbd54791b6529fa47330a8277e
savaged <david@savaged.info>
2018-08-22 15:22:42 +0100

    Working e2e

--- SixNations/SixNations.Desktop/ViewModels/MainViewModel.cs
+++ SixNations/SixNations.Desktop/ViewModels/MainViewModel.cs
@@ -2,6 +2,7 @@ using CommonServiceLocator;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
 using MvvmDialogs;
+using SixNations.Desktop.Messages;
 using System.Windows.Input;
 
 namespace SixNations.Desktop.ViewModels
@@ -12,6 +13,7 @@ namespace SixNations.Desktop.ViewModels
         {
             DialogService = dialogService;
 
+            ExitCmd = new RelayCommand(OnExit);
             ShowAboutDialogCmd = new RelayCommand(OnShowAboutDialog);
 
             if (IsInDesignMode)
@@ -22,16 +24,34 @@ namespace SixNations.Desktop.ViewModels
             {
                 // Code runs "for real"
             }
+            MessengerInstance.Register<AuthenticatedMessage>(this, OnAuthenticated);
         }
 
         public IDialogService DialogService { get; }
 
+        public ICommand ExitCmd { get; }
+
         public ICommand ShowAboutDialogCmd { get; }
+
+        public bool IsLoggedIn => !string.IsNullOrEmpty(AuthToken);
+
+        public string AuthToken { get; private set; }
+
+        private void OnAuthenticated(AuthenticatedMessage m)
+        {
+            AuthToken = m.Token;
+            RaisePropertyChanged(() => IsLoggedIn);
+        }
 
         private void OnShowAboutDialog()
         {
             var vm = ServiceLocator.Current.GetInstance<AboutDialogViewModel>();
             DialogService.ShowDialog(this, vm);
+        }
+
+        private void OnExit()
+        {
+            App.Current.Shutdown();
         }
     }
 }
