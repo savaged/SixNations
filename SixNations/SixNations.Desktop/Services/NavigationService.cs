acb8f38a5861a308fe46198d34214a9832cec611
savaged <david@savaged.info>
2018-08-23 15:47:43 +0100

    Aiming to implement INavigationService using SelectedIndex and Enum for pages

---
+++ SixNations/SixNations.Desktop/Services/NavigationService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using GalaSoft.MvvmLight.Views;
+
+namespace SixNations.Desktop.Services
+{
+    public class NavigationService : INavigationService
+    {
+        // TODO Implement navigation based on SelectedIndex and Enum for page
+        public string CurrentPageKey => throw new NotImplementedException();
+
+        public void GoBack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NavigateTo(string pageKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NavigateTo(string pageKey, object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
