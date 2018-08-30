8fdc1828e78042c3c6519352db77ce8887a405dd
savaged <david@savaged.info>
2018-08-30 11:54:35 +0100

    Busy state management

--- SixNations/SixNations.Desktop/Messages/BusyMessage.cs
+++ SixNations/SixNations.Desktop/Messages/BusyMessage.cs
@@ -4,8 +4,27 @@ using GalaSoft.MvvmLight.Messaging;
 
 namespace SixNations.Desktop.Messages
 {
+    /// <summary>
+    /// Message for updating Busy State, used in conjunction with 
+    /// SixNations.Desktop.Helpers.BusyStateManager 
+    /// </summary>
+    /// <example>
+    /// Used in conjunction with SixNations.Desktop.Helpers.BusyStateManager 
+    /// like this:
+    /// <code>
+    /// MessengerInstance.Send(new BusyMessage(true, this));
+    /// await DoSomthingAsync();
+    /// MessengerInstance.Send(new BusyMessage(false, this));
+    /// </code>
+    /// </example>
     public class BusyMessage : MessageBase
     {
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="isBusy">The state</param>
+        /// <param name="caller">The calling class</param>
+        /// <param name="callerMember">The method or property (leave blank because reflection gets this)</param>
         public BusyMessage(bool isBusy, object caller, [CallerMemberName] string callerMember = "")
         {
             IsBusy = isBusy;
