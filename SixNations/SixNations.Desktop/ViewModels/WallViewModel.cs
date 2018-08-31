de6036adc600632e55b33e0234418edb38445970
savaged <savagedavid@gmail.com>
2018-08-31 16:01:49 +0100

    Setting up for Wall swimlanes

--- SixNations/SixNations.Desktop/ViewModels/WallViewModel.cs
+++ SixNations/SixNations.Desktop/ViewModels/WallViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using SixNations.Desktop.Interfaces;
 using SixNations.Desktop.Models;
+using SixNations.Desktop.Constants;
 
 namespace SixNations.Desktop.ViewModels
 {
@@ -14,6 +17,23 @@ namespace SixNations.Desktop.ViewModels
             WIP = new ObservableCollection<Requirement>();
             Test = new ObservableCollection<Requirement>();
             Done = new ObservableCollection<Requirement>();
+        }
+
+        public async override Task LoadAsync()
+        {
+            await base.LoadAsync();
+
+            Index.Where(r => r.Status == (int)RequirementStatus.Prioritised)
+                .ToList().ForEach(r => Prioritised.Add(r));
+
+            Index.Where(r => r.Status == (int)RequirementStatus.WIP)
+                .ToList().ForEach(r => WIP.Add(r));
+
+            Index.Where(r => r.Status == (int)RequirementStatus.Test)
+                .ToList().ForEach(r => Test.Add(r));
+
+            Index.Where(r => r.Status == (int)RequirementStatus.Done)
+                .ToList().ForEach(r => Done.Add(r));
         }
 
         public ObservableCollection<Requirement> Prioritised { get; }
