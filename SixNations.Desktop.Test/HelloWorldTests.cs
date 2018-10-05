55d3e7849e82001a75974dc2cd4f14eabf8a2209
savaged <david@savaged.info>
2018-10-05 14:43:07 +0100

    Attempting to use Appium.WebAppDriver to test the Desktop app

---
+++ SixNations.Desktop.Test/HelloWorldTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SixNations.Desktop.Test
+{
+    [TestClass]
+    public class HelloWorldTests
+    {
+        [TestMethod]
+        public void HelloWorldTest()
+        {
+            Assert.IsTrue(true, "Hello World!");
+        }
+    }
+}
