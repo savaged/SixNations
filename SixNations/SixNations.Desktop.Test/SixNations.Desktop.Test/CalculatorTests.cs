98d447a0959837fea7da603952d0770c88ebf35e
savaged <david@savaged.info>
2018-10-08 08:54:53 +0100

    Setting up for adapting the sample to work with my app

--- SixNations/SixNations.Desktop.Test/SixNations.Desktop.Test/CalculatorTests.cs
+++ SixNations/SixNations.Desktop.Test/SixNations.Desktop.Test/CalculatorTests.cs
@@ -11,81 +11,13 @@ namespace SixNations.Desktop.Test
     /// Remember to run the exe (probably at C:\Program Files (x86)\Windows Application Driver)
     /// </summary>
     [TestClass]
-    public class CalculatorTests
+    public class CalculatorTests : WinAppDriverBase
     {
-        private const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
-        private const string CalculatorAppId = "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App";
-
-        protected static WindowsDriver<WindowsElement> session;
-        private static WindowsElement header;
-        private static WindowsElement calculatorResult;
-
-        [ClassInitialize()]
-        public static void ClassInit(TestContext context)
-        {
-            // Launch Calculator application if it is not yet launched
-            if (session == null)
-            {
-                // Create a new session to bring up an instance of the Calculator application
-                // Note: Multiple calculator windows (instances) share the same process Id
-                DesiredCapabilities appCapabilities = new DesiredCapabilities();
-                appCapabilities.SetCapability("app", CalculatorAppId);
-                appCapabilities.SetCapability("deviceName", "WindowsPC");
-                session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appCapabilities);
-                Assert.IsNotNull(session);
-
-                // Set implicit timeout to 1.5 seconds to make element search to retry every 500 ms for at most three times
-                session.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(1.5));
-            }
-
-            try
-            {
-                header = session.FindElementByAccessibilityId("Header");
-            }
-            catch
-            {
-                header = session.FindElementByAccessibilityId("ContentPresenter");
-            }
-
-            // Ensure that calculator is in standard mode
-            if (!header.Text.Equals("Standard", StringComparison.OrdinalIgnoreCase))
-            {
-                session.FindElementByAccessibilityId("NavButton").Click();
-                Thread.Sleep(TimeSpan.FromSeconds(1));
-                var splitViewPane = session.FindElementByClassName("SplitViewPane");
-                splitViewPane.FindElementByName("Standard Calculator").Click();
-                Thread.Sleep(TimeSpan.FromSeconds(1));
-                Assert.IsTrue(header.Text.Equals("Standard", StringComparison.OrdinalIgnoreCase));
-            }
-
-            // Locate the calculatorResult element
-            calculatorResult = session.FindElementByAccessibilityId("CalculatorResults");
-            Assert.IsNotNull(calculatorResult);
-        }
-
-        [ClassCleanup]
-        public static void CleanupClass()
-        {
-            // Close the application and delete the session
-            if (session != null)
-            {
-                session.Quit();
-                session = null;
-            }
-        }
-
-
         [TestInitialize()]
         public void Initialize()
         {
             session.FindElementByName("Clear").Click();
             Assert.AreEqual("0", GetCalculatorResultText());
-        }
-
-        [TestCleanup]
-        public void CleanupTest()
-        {
-
         }
 
         [TestMethod]
@@ -146,11 +78,6 @@ namespace SixNations.Desktop.Test
             session.FindElementByName(input2).Click();
             session.FindElementByName("Equals").Click();
             Assert.AreEqual(expectedResult, GetCalculatorResultText());
-        }
-
-        private string GetCalculatorResultText()
-        {
-            return calculatorResult.Text.Replace("Display is", string.Empty).Trim();
         }
     }
 }
