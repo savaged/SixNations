0a34f3a0cf29339756a0db096c8988cdb73ee20c
savaged <david@savaged.info>
2018-10-08 10:48:09 +0100

    Actually these are tests at the data service level so will work on VM tests now

--- SixNations/SixNations.Desktop.Test/SixNations.Desktop.Test/RequirementTests.cs
+++ SixNations/SixNations.Desktop.Test/SixNations.Desktop.Test/RequirementTests.cs
@@ -12,7 +12,7 @@ namespace SixNations.Desktop.Test
     [TestClass]
     public class RequirementTests
     {
-        private static IDataService<Requirement> _requirementsDataService;
+        private static IDataService<Requirement> _dataService;
 
         [ClassInitialize]
         public static void Init(TestContext context)
@@ -24,14 +24,22 @@ namespace SixNations.Desktop.Test
             SimpleIoc.Default.Register<IDataService<Lookup>, LookupDataService>();
             SimpleIoc.Default.Register<IDataService<Requirement>, RequirementDataService>();
 
-            _requirementsDataService = SimpleIoc.Default
+            _dataService = SimpleIoc.Default
                 .GetInstance<IDataService<Requirement>>();
         }
 
         [TestMethod]
-        public async Task IndexTest()
+        public async Task CreateTest()
         {
-            var index = await _requirementsDataService.GetModelDataAsync(
+            var item = await _dataService.CreateModelAsync(
+                "mockToken", (ex) => throw ex);
+            Assert.IsNotNull(item);
+        }
+
+        [TestMethod]
+        public async Task ReadTest()
+        {
+            var index = await _dataService.GetModelDataAsync(
                 "mockToken", (ex) => throw ex);
             Assert.IsNotNull(index);
             Assert.AreEqual(6, index.ToList().Count, "The count of mocked requirements");
