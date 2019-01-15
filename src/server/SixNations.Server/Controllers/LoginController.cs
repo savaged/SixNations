c24ef9de07df6888ee87c1e1fde637568180253c
David Savage <david@savaged.info>
2019-01-15 12:01:59 +0000

    i did have the login working but forgot to commit :(

--- src/server/SixNations.Server/Controllers/LoginController.cs
+++ src/server/SixNations.Server/Controllers/LoginController.cs
@@ -4,23 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SixNations.Server.Data;
+using SixNations.Server.Services;
 
 namespace SixNations.Server.Controllers
 {
+    // TODO set-up and integrate a standard identity server like identityserver4 or
+    // something like the ASP.Net Core auth demo:
+    // https://github.com/aspnet/Docs/tree/master/aspnetcore/security/authentication/identity/sample/src/ASPNETCore-IdentityDemoComplete/IdentityDemo
+    // Then remove all my 'home-baked' auth stuff
     [Route("api/[controller]")]
     [ApiController]
     public class LoginController : ControllerBase
     {
+        private readonly IAuthService _auth;
+
+        public LoginController(IAuthService auth)
+        {
+            _auth = auth;
+        }
+
         // POST: api/Login
         [HttpPost]
         public async Task<IActionResult> PostLogin()
         {
-            // TODO set-up and integrate a standard identity server like identityserver4 or
-            // something like the ASP.Net Core auth demo:
-            // https://github.com/aspnet/Docs/tree/master/aspnetcore/security/authentication/identity/sample/src/ASPNETCore-IdentityDemoComplete/IdentityDemo
+            var username = "todo";
+            var password = "todo";
 
-            await Task.CompletedTask;
-            return Ok("todo");
+            var authUser = await _auth.AuthenticateAsync(username, password);
+            return Ok(authUser.access_token);
         }
     }
 }
